6b956ea387b26e5a7c6fda975f99273cedcf210e
leozamarron <zamarronleo4@gmail.com>
2024-09-18 23:45:10 -0600

    Eliminacion de elemntos y bloqueo de eliminacion de elementos

--- examen1_zrl/lib/main.dart
+++ examen1_zrl/lib/main.dart
@@ -1,6 +1,6 @@
+import 'package:examen1_zrl/login.dart';
 import 'package:flutter/material.dart';
-//import 'package:examen1_zrl/login.dart';
-import 'package:examen1_zrl/home.dart';
+//import 'package:examen1_zrl/home.dart';
 
 void main() {
   runApp(const MyApp());
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Home(),
+      home: const Login(),
     );
   }
 }
